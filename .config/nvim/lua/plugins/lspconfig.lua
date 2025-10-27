6188ae8a0319967321d125b0374fce2e97918f94
Amhed Rami <ahmed.rami.company@gmail.com>
2025-10-27 02:44:46 +0200

    feat(nvim): updates deprecated config in lspconfig package

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,8 +1,7 @@
 return {
   "neovim/nvim-lspconfig",
-  opts = function()
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    keys[#keys + 1] = { "gy", false }
-    keys[#keys + 1] = { "<leader>cc", false }
-  end,
+  keys = {
+    { "gy", false },
+    { "<leader>cc", false },
+  },
 }
